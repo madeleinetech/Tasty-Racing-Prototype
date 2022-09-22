809b64ee6a241717633331eb6a2b7e6ef2764290
madeleinetech <60237307+madeleinetech@users.noreply.github.com>
2022-09-22 10:56:04 -0400

    Xbox Moves the Player now

--- Assets/MovementX.cs
+++ Assets/MovementX.cs
@@ -7,30 +7,32 @@ public class MovementX : MonoBehaviour
 {
     private Rigidbody prb;
     private PlayerInput playerInput;
-    private PlayerInputActions playerInputActions;
+    private XboxControls playerInputActions;
 
     private void Awake()
     { 
         prb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
 
-        playerInputActions = new PlayerInputActions();
+        playerInputActions = new XboxControls();
         playerInputActions.Player.Enable();
         playerInputActions.Player.Movement.performed += Up;
     }
 
     private void Update()
     {
+        Debug.Log("it worked!");
         Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
-        float speed = 100f;
+        float speed = 1000f;
         prb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode.Force);
     }
 
     public void Up(InputAction.CallbackContext context)
     {
+        Debug.Log("Up worked!");
         //Vector3 tempVect = new Vector3(0, 0, 1);
         Vector2 inputVector = context.ReadValue<Vector2>();
-        float speed = 100f;
+        float speed = 1000f;
         prb.AddForce(new Vector3(inputVector.x, 0, inputVector.y) * speed, ForceMode.Force);
         //tempVect = tempVect.normalized * speed * Time.deltaTime;
         //prb.MovePosition(transform.position + tempVect);
