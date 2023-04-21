dc55dda8d6a39a4ea6c8d5b5dda7e7e32b61b682
madeleinetech <60237307+madeleinetech@users.noreply.github.com>
2023-04-21 16:02:25 -0400

    Animation changes

--- Assets/WASDmove.cs
+++ Assets/WASDmove.cs
@@ -3,28 +3,29 @@ using UnityEngine;
 public class WASDmove : MonoBehaviour
 {
     public float speed = 0.1f;
+    public float rotationSpeed = 0.1f;
     // Update is called once per frame
     void Update()
     {
         //add force (fixed update)
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * speed);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * speed);
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.up, -1);
+            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.up, 1);
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
         }
     }
 }
